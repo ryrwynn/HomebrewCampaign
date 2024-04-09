d4ea77f1e367e4d29817305a59d63213d279ab55
ryrwynn <irene.ccchung@gmail.com>
2024-04-09 09:34:35 -0700

    player select
    working local storage player select

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("#theo").addEventListener("click", function(){
+    localStorage.setItem("player", "theo");
+})
+
+document.querySelector("#leyna").addEventListener("click", function(){
+    localStorage.setItem("player", "leyna");
+})
+
+document.querySelector("#mahba").addEventListener("click", function(){
+    localStorage.setItem("player", "mahba");
+})
